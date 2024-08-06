9bd463345ea7fafd5475254a5a3203851f23d03e
Frederico Swarthings <timscallon1@gmail.com>
2024-08-06 00:07:52 -0500

    got tables working and started on test data seeding

--- db/schema.sql
+++ db/schema.sql
@@ -1,9 +1,25 @@
-DROP DATABASE IF EXISTS courses_db;
-CREATE DATABASE courses_db;
+\c postgres;
+DROP DATABASE IF EXISTS employees_db;
+CREATE DATABASE employees_db;
 
-\c courses_db;
+\c employees_db;
 
-CREATE TABLE course_names (
+CREATE TABLE department (
   id SERIAL PRIMARY KEY,
-  name VARCHAR(30) NOT NULL
-)
+  name VARCHAR(30) UNIQUE NOT NULL
+);
+
+CREATE TABLE role (
+  id SERIAL PRIMARY KEY,
+  title VARCHAR(30) UNIQUE NOT NULL,
+  salary DECIMAL NOT NULL,
+  department_id INTEGER NOT NULL --to hold reference to department role belongs to
+);
+
+CREATE TABLE employee (
+  id SERIAL PRIMARY KEY,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  role_id INTEGER NOT NULL, --to hold reference to role of the employee
+  manager_id INTEGER --reference to another employee that is the manager of the current employee (null if the employee has no manager)
+);
