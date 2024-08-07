6db7b2943ac4b90f4c95e58cdb3a85362fea0433
Frederico Swarthings <timscallon1@gmail.com>
2024-08-06 20:30:17 -0500

    added some foreign key stuff to my tables as they were not talking at all. I will need to nuke and rebuild them.

--- db/schema.sql
+++ db/schema.sql
@@ -13,13 +13,21 @@ CREATE TABLE role (
   id SERIAL PRIMARY KEY,
   title VARCHAR(30) UNIQUE NOT NULL,
   salary DECIMAL NOT NULL,
-  department_id INTEGER NOT NULL --to hold reference to department role belongs to
+  department_id INTEGER NOT NULL
+  FOREIGN KEY (department_id)
+    REFERENCES department(id)
+    ON DELETE SET NULL
 );
 
 CREATE TABLE employee (
   id SERIAL PRIMARY KEY,
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR(30) NOT NULL,
-  role_id INTEGER NOT NULL, --to hold reference to role of the employee
-  manager_id INTEGER --reference to another employee that is the manager of the current employee (null if the employee has no manager)
+  role_id INTEGER NOT NULL
+  FOREIGN KEY (role_id)
+    REFERENCES role(id)
+    ON DELETE SET NULL,
+  manager_id INTEGER DEFAULT 0
+  FOREIGN KEY (manager_id)
+    REFERENCES manager(id)
 );
