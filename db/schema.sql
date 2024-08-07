0de92b5cba25817efd3f8f6942dcd196717500cf
Frederico Swarthings <timscallon1@gmail.com>
2024-08-06 21:02:05 -0500

    vastly improved schema and seed files

--- db/schema.sql
+++ db/schema.sql
@@ -3,31 +3,34 @@ DROP DATABASE IF EXISTS employees_db;
 CREATE DATABASE employees_db;
 
 \c employees_db;
-
+DROP TABLE IF EXISTS department;
 CREATE TABLE department (
   id SERIAL PRIMARY KEY,
   name VARCHAR(30) UNIQUE NOT NULL
 );
 
+DROP TABLE IF EXISTS role;
 CREATE TABLE role (
   id SERIAL PRIMARY KEY,
   title VARCHAR(30) UNIQUE NOT NULL,
   salary DECIMAL NOT NULL,
-  department_id INTEGER NOT NULL
+  department_id INTEGER NOT NULL,
   FOREIGN KEY (department_id)
     REFERENCES department(id)
-    ON DELETE SET NULL
+    ON DELETE CASCADE
 );
 
+DROP TABLE IF EXISTS employee;
 CREATE TABLE employee (
   id SERIAL PRIMARY KEY,
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR(30) NOT NULL,
-  role_id INTEGER NOT NULL
+  role_id INTEGER NOT NULL,
   FOREIGN KEY (role_id)
     REFERENCES role(id)
-    ON DELETE SET NULL,
-  manager_id INTEGER DEFAULT 0
+    ON DELETE CASCADE,
+  manager_id INTEGER DEFAULT NULL,
   FOREIGN KEY (manager_id)
-    REFERENCES manager(id)
+    REFERENCES employee(id)
+    ON DELETE SET NULL
 );
