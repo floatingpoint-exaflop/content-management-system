6ad5b4189f2a1937ee7dd2b313647f6d671e4412
Frederico Swarthings <timscallon1@gmail.com>
2024-08-06 20:18:31 -0500

    completed vast majority of js work including inquirer prompts; cannot really test yet as db needs some major schema rewrites

--- index.js
+++ index.js
@@ -1,13 +1,69 @@
-const pg = require('pg');
-const inquirer = require('inquirer')
-const MaxLengthInputPrompt = require('inquirer-maxlength-input-prompt')
-inquirer.registerPrompt('maxlength-input', MaxLengthInputPrompt)
+//-------------------------------------------------------
+const pool = require('pg');
 
 const {Department} = require("./lib/department");
 const {Employee} = require("./lib/employee");
 const {Role} = require("./lib/role");
 
-
+pool.connect();
+//-------------------------------------------------------
+//viewAll functions
+//-------------------------------------------------------
+async function viewAllEmployees(){
+    const employees = await pool.query('SELECT * FROM employees');
+    console.table(employees);
+    start();
+}
+//-------------------------------------------------------
+async function viewAllDepartments(){
+    const departments = await pool.query('SELECT * FROM departments');
+    console.table(departments);
+    start();
+}
+//-------------------------------------------------------
+async function viewAllRoles(){
+    const roles = await pool.query('SELECT * FROM roles');
+    console.table(roles);
+    start();
+}
+//-------------------------------------------------------
+//addNew functions
+//-------------------------------------------------------
+async function addNewEmployee(){
+    const employeeInst = new Employee;
+    const data = await employeeInst.addEmployee();
+    const insert = await pool.query('INSERT INTO employees(first_name, last_name, role_id, manager_id) VALUES($1, $2, $3, $4)', [data.firstName, data.lastName, data.roleId, data.managerId]);
+    console.log(`New ${data.roleId} ${data.firstName} ${data.lastName} added!`);
+    start();
+}
+//-------------------------------------------------------
+async function addNewDepartment(){
+    const deptInst = new Department;
+    const data = await deptInst.addDepartment();
+    const insert = await pool.query('INSERT INTO departments(name) VALUES($1)', [data.name]);
+    console.log(`New ${data.name} Department added!`);
+    start();
+}
+//-------------------------------------------------------
+async function addNewRole(){
+    const roleInst = new Role;
+    const data = await roleInst.addRole();
+    const insert = await pool.query('INSERT INTO employees(title, salary, department_id) VALUES($1, $2, $3)', [data.title, data.salary, data.departmentId]);
+    console.log(`New employee role ${data.title} has been added; the salart is ${data.salary}!`);
+    start();
+}
+//-------------------------------------------------------
+//updateEmployeeRole function
+//-------------------------------------------------------
+async function updateEmployeeRole(){
+    const employeeInst = new Employee;
+    const data = await employeeInst.updateEmployee();
+    const update = await pool.query('????', [data.firstName, data.lastName, data.newRoleId, data.newManagerId]);
+    console.log(`${data.firstName} ${data.lastName} is now listed as a ${data.newRoleId} reporting to ${data.newManagerId}!`);
+    start();
+}
+//-------------------------------------------------------
+//-------------------------------------------------------
 function start(){
     inquirer.prompt([
         {type: "list",
@@ -17,23 +73,19 @@ function start(){
         }
     ]).then( ({options}) =>{
         if(options === "View All Departments"){
-            SELECT * FROM departments;
+            viewAllDepartments();
         } else if(options === "View All Roles"){
-            SELECT * FROM roles;
+            viewAllRoles();
         } else if(options === "View All Employees"){
-            SELECT * FROM employees;
+            viewAllEmployees();
         } else if(options === "Add a Department"){
-            const department = new Department()
-            askAboutAdd(department)
+            addNewDepartment();
         } else if(options === "Add a Role"){
-            const role = new Role()
-            askAboutAdd(role)
+            addNewRole();
         } else if(options === "Add an Employee"){
-            const employee = new Employee()
-            askAboutAdd(employee)
+            addNewEmployee();
         } else if(options === "Update an Employee Role"){
-            // const cart = new Cart()
-            // askAboutAdd(cart)
+            updateEmployeeRole();
         } else {
             console.log("Have a nice day!")
             process.exit()//this terminates the entire process and cancels anything downstream in the same process
@@ -41,88 +93,4 @@ function start(){
     })
 }
 
-function askAboutAdd(cart){
-    inquirer.prompt([
-        {
-            type: "list",
-            message: "Would you like to add a product?",
-            name: "okToAdd",
-            choices: ["yes", "no"]
-        }
-    ]).then( ({okToAdd}) => {
-        if(okToAdd === "yes"){
-        cart.addProduct() 
-    } else {
-            console.log("Have a nice day!")
-            process.exit()
-            }
-}
-    )
-}
-
 start()
-
-
-//questions; note that for colors, the user is unable to enter anything that isn't a valid hex or html named color.
-function userShapeQ(){
-    return [
-    {
-        type: 'maxlength-input',
-        message: "Let's make an svg! You can enter up to three characters of text:",
-        name: 'text',
-        maxLength: 3
-    },
-    {
-        type: "input",
-        name: "textcolor",
-        message: "What color should the text be? Please give an HTML color name or hexcode.",
-        validate: (textcolor) => {
-            if (isHexColor(textcolor) || isNamedColor(textcolor)) {
-                return true;
-            }
-            return 'Not a valid HTML color name or hex code; please try again.'
-        }
-    },
-    {
-         type: "list",
-         message: "What shape do you want?",
-         name: "shape",
-         choices: ["Circle","Triangle","Square"]
-    },
-    {
-        type: "input",
-        name: "shapecolor",
-        message: "What color should the shape be? Please give an HTML color name or hexcode.",
-        validate: (shapecolor) => {
-            if (isHexColor(shapecolor) || isNamedColor(shapecolor)) {
-                return true;
-            }
-            return 'Not a valid HTML color name or hex code; please try again.'
-        }
-    }
- ]};
-
-
-//The main function: this brings in the questions and calls the correct class constructor for the chosen shape, which is created with the other chosen properties.
-async function init(){
-    const questions = userShapeQ();
-    const answers = await inquirer.prompt(questions);
-    let shape;
-    if (answers.shape === "Circle"){
-        shape = new Circle(answers.shapecolor, answers.text, answers.textcolor)
-    } else if (answers.shape === "Triangle"){
-        shape = new Triangle(answers.shapecolor, answers.text, answers.textcolor)
-    } else if (answers.shape === "Square"){
-        shape = new Square(answers.shapecolor, answers.text, answers.textcolor)
-    }
-    const outputSVG = shape.render();
-    fs.writeFile('logo.svg', outputSVG, (err) =>{
-        if (err){
-            console.error("The SVG file could not be generated; you aren't much of a painter...")
-        } else {
-            console.log("SVG file successfully generated from the information collected. You're an artist!")
-        }
-    })
-};
-
-init();
